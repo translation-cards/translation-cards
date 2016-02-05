8bdf23951767a107e599c6d0093a6a17d8734085
natashaj7 <njimenez@thoughtworks.com>
2016-02-05 13:41:26 -0600

    #3 Nat Fixed back arrow color on ActionBar

--- app/src/main/java/org/mercycorps/translationcards/TranslationsActivity.java
+++ app/src/main/java/org/mercycorps/translationcards/TranslationsActivity.java
@@ -28,7 +28,6 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
