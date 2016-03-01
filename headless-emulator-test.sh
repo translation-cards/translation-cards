dc13aca87b0f4257c977c1400ca28639fa5c5564
Pavlina Lejskova <pavlina.lejskova@gmail.com>
2016-03-01 16:21:02 -0600

    Pat/Pav Add android-wait-for-emulator #56

--- headless-emulator-test.sh
+++ headless-emulator-test.sh
@@ -2,6 +2,6 @@
 
 echo no | android create avd --force -n test -t android-10 --abi armeabi-v7a
 emulator -avd test -no-skin -no-audio -no-window &
-./android-wait-for-emulator # see https://github.com/gildegoma/chef-android-sdk/blob/master/files/default/android-wait-for-emulator
+./android-wait-for-emulator.sh # see https://github.com/gildegoma/chef-android-sdk/blob/master/files/default/android-wait-for-emulator
 adb shell input keyevent 82 &
 ./gradlew connectedAndroidTest
