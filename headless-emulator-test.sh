440a50db2dd215978beaa5c408d594b05eab8594
Pavlina Lejskova <pavlina.lejskova@gmail.com>
2016-03-01 15:52:16 -0600

    Pat/Pav Add script for headless emulator tests #56

---
+++ headless-emulator-test.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+echo no | android create avd --force -n test -t android-10 --abi armeabi-v7a
+emulator -avd test -no-skin -no-audio -no-window &
+./android-wait-for-emulator # see https://github.com/gildegoma/chef-android-sdk/blob/master/files/default/android-wait-for-emulator
+adb shell input keyevent 82 &
+./gradlew connectedAndroidTest
