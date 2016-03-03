96123015156832a00977fef94390f5b42c5aaa0e
Pavlina Lejskova <pavlina.lejskova@gmail.com>
2016-03-03 13:34:13 -0600

    #56 Pav Refactor user flow test and add the step of deleting a card so that there is some kind of test clean up.

--- app/src/androidTest/java/org.mercycorps.translationcards/UserFlow.java
+++ app/src/androidTest/java/org.mercycorps.translationcards/UserFlow.java
@@ -14,6 +14,8 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.replaceText;
 import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.PositionAssertions.isRightOf;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static java.lang.Thread.sleep;
@@ -24,24 +26,34 @@ public class UserFlow {
 
     public static final String ORIGIN_TEXT = "Where does it hurt?";
     public static final String TRANSLATION_TEXT = "حيث أنها لا تؤذي؟";
+
     @Rule
     public ActivityTestRule<DecksActivity> activityTestRule = new ActivityTestRule<>(DecksActivity.class);
 
     @Test
-    public void shouldFindTheDefaultDeck() throws InterruptedException {
+    public void createAndDeleteCardFlow() throws InterruptedException {
+
         onView(withText("Default")).perform(click());
+
         onView(withId(R.id.add_button)).perform(click());
         onView(withId(R.id.recording_instructions_start)).perform(click());
+
         onView(withText("Flashcard phrase")).perform(typeText(ORIGIN_TEXT));
         closeSoftKeyboard();
         onView(withText("Phrase translation (Optional)")).perform(replaceText(TRANSLATION_TEXT));
         onView(withId(R.id.recording_label_next)).perform(click());
+
         onView(withId(R.id.recording_audio_button_record)).perform(click());
         sleep(2000);
         onView(withId(R.id.recording_audio_button_record)).perform(click());
         onView(withId(R.id.recording_audio_save)).perform(click());
-        onView(withText(ORIGIN_TEXT));
-        onView(withText(TRANSLATION_TEXT));
+
+        onView(withId(R.id.origin_translation_text)).check(matches(withText(ORIGIN_TEXT)));
+        onView(withId(R.id.translated_text)).check(matches(withText(TRANSLATION_TEXT)));
         onView(withId(R.id.recording_done)).perform(click());
+
+        onView(withId(R.id.indicator_icon)).check(isRightOf(withText(ORIGIN_TEXT))).perform(click());
+        onView(withText("Delete this flashcard")).perform(click());
+        onView(withText("OK")).perform(click());
     }
 }
