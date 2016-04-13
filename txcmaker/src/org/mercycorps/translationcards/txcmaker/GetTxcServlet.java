fb98a412e0b24216ef7eb768389546b1b0628412
Nick Worden <nick.c.worden@gmail.com>
2016-04-13 04:09:42 -0400

    Super-basic Drive stuff.

--- txcmaker/src/org/mercycorps/translationcards/txcmaker/GetTxcServlet.java
+++ txcmaker/src/org/mercycorps/translationcards/txcmaker/GetTxcServlet.java
@@ -1,6 +1,16 @@
 package org.mercycorps.translationcards.txcmaker;
 
+import com.google.api.client.googleapis.extensions.appengine.auth.oauth2.AppIdentityCredential;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.DriveScopes;
+import com.google.api.services.drive.model.File;
+
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -8,15 +18,28 @@ import javax.servlet.http.HttpServletResponse;
 
 public class GetTxcServlet extends HttpServlet {
 
+  private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+
+  private static HttpTransport httpTransport;
+
   @Override
   public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-    Map<String, List<BlobKey>> blobs = blobstoreService.getUploads(req);
-    List<BlobKey> blobKeys = blobs.get("specFile");
-    if (blobKeys == null || blobKeys.isEmpty()) {
-      res.sendRedirect("/");
-    } else {
-      res.sendRedirect("/serve?blob-key=" + blobKeys.get(0).getKeyString());
+    Drive drive = getDriveService();
+    resp.getWriter().println("Hello. Here are some filenames.");
+    List<File> files = drive.files().list()
+        .setMaxResults(10)
+        .setFields("nextPageToken, files(id, name)")
+        .execute().getItems();
+    for (File file : files) {
+      resp.getWriter().println(file.getTitle());
     }
-    resp.getWriter().println("Hello. Here is your TXC.");
+  }
+
+  private Drive getDriveService() {
+    AppIdentityCredential credential = new AppIdentityCredential(
+        Arrays.asList(DriveScopes.DRIVE_READONLY));
+    return new Drive.Builder(httpTransport, JSON_FACTORY, credential)
+        .setApplicationName("TXC Maker")
+        .build();
   }
 }
