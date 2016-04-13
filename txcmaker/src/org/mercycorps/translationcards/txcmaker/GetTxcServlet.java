7b58ff5ca99e727e4375c61cd60106be54bae4bc
Nick Worden <nick.c.worden@gmail.com>
2016-04-13 04:32:52 -0400

    Works but doesn't.

--- txcmaker/src/org/mercycorps/translationcards/txcmaker/GetTxcServlet.java
+++ txcmaker/src/org/mercycorps/translationcards/txcmaker/GetTxcServlet.java
@@ -1,6 +1,7 @@
 package org.mercycorps.translationcards.txcmaker;
 
 import com.google.api.client.googleapis.extensions.appengine.auth.oauth2.AppIdentityCredential;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.json.JsonFactory;
@@ -9,6 +10,7 @@ import com.google.api.services.drive.DriveScopes;
 import com.google.api.services.drive.model.File;
 
 import java.io.IOException;
+import java.security.GeneralSecurityException;
 import java.util.Arrays;
 import java.util.List;
 
@@ -20,15 +22,17 @@ public class GetTxcServlet extends HttpServlet {
 
   private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
 
-  private static HttpTransport httpTransport;
-
   @Override
   public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
     Drive drive = getDriveService();
+    if (drive == null) {
+      resp.getWriter().println("Error connecting to Drive.");
+      return;
+    }
     resp.getWriter().println("Hello. Here are some filenames.");
     List<File> files = drive.files().list()
         .setMaxResults(10)
-        .setFields("nextPageToken, files(id, name)")
+        .setFields("nextPageToken, items(id, title)")
         .execute().getItems();
     for (File file : files) {
       resp.getWriter().println(file.getTitle());
@@ -36,6 +40,14 @@ public class GetTxcServlet extends HttpServlet {
   }
 
   private Drive getDriveService() {
+    HttpTransport httpTransport;
+    try {
+      httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+    } catch (GeneralSecurityException e) {
+      return null;
+    } catch (IOException e) {
+      return null;
+    }
     AppIdentityCredential credential = new AppIdentityCredential(
         Arrays.asList(DriveScopes.DRIVE_READONLY));
     return new Drive.Builder(httpTransport, JSON_FACTORY, credential)
