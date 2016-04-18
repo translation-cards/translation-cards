3bc69c4618787937a37c55fb967fccf24087ae8a
Nick Worden <nick.c.worden@gmail.com>
2016-04-18 18:33:04 -0400

    Just saving what I have. It's probably bad.

--- txcmaker/src/org/mercycorps/translationcards/txcmaker/GetTxcServlet.java
+++ txcmaker/src/org/mercycorps/translationcards/txcmaker/GetTxcServlet.java
@@ -1,29 +1,39 @@
 package org.mercycorps.translationcards.txcmaker;
 
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.appengine.datastore.AppEngineDataStoreFactory;
+import com.google.api.client.extensions.servlet.auth.oauth2.AbstractAuthorizationCodeServlet;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.extensions.appengine.auth.oauth2.AppIdentityCredential;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.json.JsonFactory;
+import com.google.api.client.util.store.DataStoreFactory;
 import com.google.api.services.drive.Drive;
 import com.google.api.services.drive.DriveScopes;
 import com.google.api.services.drive.model.File;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class GetTxcServlet extends HttpServlet {
+public class GetTxcServlet extends AbstractAuthorizationCodeServlet {
 
   private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+  private static final DataStoreFactory DATA_STORE_FACTORY =
+      AppEngineDataStoreFactory.getDefaultInstance();
 
   @Override
-  public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+  public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
     Drive drive = getDriveService();
     if (drive == null) {
       resp.getWriter().println("Error connecting to Drive.");
@@ -39,6 +49,13 @@ public class GetTxcServlet extends HttpServlet {
     }
   }
 
+  @Override
+  protected String getRedirectUri(HttpServletRequest req) throws ServletException, IOException {
+    GenericUrl url = new GenericUrl(req.getRequestURL().toString());
+    url.setRawPath("/oauth2callback");
+    return url.build();
+  }
+
   private Drive getDriveService() {
     HttpTransport httpTransport;
     try {
@@ -48,10 +65,27 @@ public class GetTxcServlet extends HttpServlet {
     } catch (IOException e) {
       return null;
     }
-    AppIdentityCredential credential = new AppIdentityCredential(
-        Arrays.asList(DriveScopes.DRIVE_READONLY));
+    Collection<String> scopes = Arrays.asList(DriveScopes.DRIVE_READONLY);
+    Credential credential;
+    try {
+        credential = getCredential(httpTransport, scopes);
+    } catch (IOException e) {
+      return null;
+    }
     return new Drive.Builder(httpTransport, JSON_FACTORY, credential)
         .setApplicationName("TXC Maker")
+        .build();
+  }
+
+  private Credential getCredential(HttpTransport httpTransport, Collection<String> scopes)
+      throws IOException {
+    InputStream in = GetTxcServlet.class.getResourceAsStream("/drive_client_secrets.json");
+    GoogleClientSecrets secrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+    GoogleAuthorizationCodeFlow flow =
+        new GoogleAuthorizationCodeFlow.Builder(
+            httpTransport, JSON_FACTORY, secrets, scopes)
+        .setDataStoreFactory(DATA_STORE_FACTORY)
+        .setAccessType("offline")
         .build();
   }
 }
