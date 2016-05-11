e8f7f41d1cded14e2284b6ee7cdf8e5281c857b6
Austin Garrard <agarrard@thoughtworks.com>
2016-05-11 10:54:18 -0500

    Austin #155 Add skeleton for Review And Save screen

---
+++ app/src/main/java/org/mercycorps/translationcards/activity/addDeck/ReviewAndSaveActivity.java
@@ -0,0 +1,21 @@
+package org.mercycorps.translationcards.activity.addDeck;
+
+
+import org.mercycorps.translationcards.R;
+
+import butterknife.OnClick;
+
+public class ReviewAndSaveActivity extends AddDeckActivity {
+    @Override
+    public void inflateView() {
+
+    }
+
+    @OnClick(R.id.deck_source_language_next_label)
+    protected void saveButtonClicked() {
+    }
+
+    @OnClick(R.id.deck_source_language_back_arrow)
+    public void backButtonClicked(){
+    }
+}
