5da21ce5d3b58a0f5e4ca1aa27925f1ce5633ce5
Austin Garrard <agarrard@thoughtworks.com>
2016-05-11 10:24:34 -0500

    Austin #155 Add skeleton for Author And Lock screen

---
+++ app/src/main/java/org/mercycorps/translationcards/activity/addDeck/AuthorAndLockActivity.java
@@ -0,0 +1,24 @@
+package org.mercycorps.translationcards.activity.addDeck;
+
+import org.mercycorps.translationcards.R;
+import org.mercycorps.translationcards.activity.MyDecksActivity;
+
+import butterknife.OnClick;
+
+public class AuthorAndLockActivity extends AddDeckActivity{
+
+    @Override
+    public void inflateView() {
+
+    }
+
+    @OnClick(R.id.deck_author_and_lock_next_label)
+    protected void getStartedButtonClicked() {
+    }
+
+    @OnClick(R.id.deck_author_and_lock_back)
+    public void getStartedBackButtonClicked(){
+    }
+
+
+}
