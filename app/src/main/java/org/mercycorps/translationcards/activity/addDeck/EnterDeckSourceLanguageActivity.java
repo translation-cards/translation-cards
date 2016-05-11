c54151c5b5226896b3306001fe629e43e7df6f10
Austin Garrard <agarrard@thoughtworks.com>
2016-05-11 10:42:24 -0500

    Austin #155 Add skeleton for Enter Source Language screen

---
+++ app/src/main/java/org/mercycorps/translationcards/activity/addDeck/EnterDeckSourceLanguageActivity.java
@@ -0,0 +1,22 @@
+package org.mercycorps.translationcards.activity.addDeck;
+
+
+import org.mercycorps.translationcards.R;
+
+import butterknife.OnClick;
+
+public class EnterDeckSourceLanguageActivity extends AddDeckActivity {
+    @Override
+    public void inflateView() {
+
+    }
+
+    @OnClick(R.id.deck_source_language_next_label)
+    protected void nextButtonClicked() {
+    }
+
+    @OnClick(R.id.deck_source_language_back_arrow)
+    public void backButtonClicked(){
+    }
+
+}
