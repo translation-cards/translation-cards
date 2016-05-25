8505e95326273567549e879f3e47d9e83a926211
Ben Ridge <ben@wanderaround.com>
2016-05-25 22:47:00 +0300

    Tests and fix for Issue #204: Review not showing deck info

--- app/src/main/java/org/mercycorps/translationcards/activity/addDeck/ReviewAndSaveActivity.java
+++ app/src/main/java/org/mercycorps/translationcards/activity/addDeck/ReviewAndSaveActivity.java
@@ -1,12 +1,30 @@
 package org.mercycorps.translationcards.activity.addDeck;
 
 
+import android.os.Bundle;
+import android.widget.TextView;
+
 import org.mercycorps.translationcards.R;
 import org.mercycorps.translationcards.activity.MyDecksActivity;
 
+import butterknife.Bind;
 import butterknife.OnClick;
 
 public class ReviewAndSaveActivity extends AddDeckActivity {
+    @Bind(R.id.deck_name)TextView deckNameTextView;
+    @Bind(R.id.deck_information)TextView deckInfoTextView;
+    @Bind(R.id.translation_languages)TextView deckLanguagesTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        NewDeckContext newDeckContext = getContextFromIntent();
+        deckNameTextView.setText(newDeckContext.getDeckLabel());
+        deckInfoTextView.setText(newDeckContext.getAuthor());
+        deckLanguagesTextView.setText(newDeckContext.getLanguagesInput());
+    }
+
     @Override
     public void inflateView() {
         setContentView(R.layout.activity_deck_review_and_save);
