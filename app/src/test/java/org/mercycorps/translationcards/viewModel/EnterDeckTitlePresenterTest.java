cfe24050336dd76dbd64e0868d2f930ec9183cdd
Pat Dale <pdale@thoughtworks.com>
2016-08-26 14:45:57 -0500

    Austin/Pat example test for mvp

--- app/src/test/java/org/mercycorps/translationcards/viewModel/EnterDeckTitlePresenterTest.java
+++ app/src/test/java/org/mercycorps/translationcards/viewModel/EnterDeckTitlePresenterTest.java
@@ -5,25 +5,40 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import org.junit.Before;
+import org.junit.Test;
 import org.mercycorps.translationcards.activity.addDeck.EnterDeckTitleActivity;
 import org.mercycorps.translationcards.activity.addDeck.NewDeckContext;
 
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class EnterDeckTitlePresenterTest {
 
     private static final String NO_DECK_TITLE = "";
     private static final String A_DECK_TITLE = "a deck title";
     private NewDeckContext newDeckContext;
-    private EnterDeckTitlePresenter viewModel;
+    private EnterDeckTitlePresenter presenter;
+    private TextView deckTitleInput;
 
     @Before
     public void setUp() throws Exception {
         newDeckContext = mock(NewDeckContext.class);
-        viewModel = new EnterDeckTitlePresenter(mock(EnterDeckTitleActivity.class), newDeckContext, mock(TextView.class), mock(LinearLayout.class), mock(TextView.class), mock(ImageView.class));
+        deckTitleInput = mock(TextView.class);
+        presenter = new EnterDeckTitlePresenter(mock(EnterDeckTitleActivity.class), newDeckContext, deckTitleInput, mock(LinearLayout.class), mock(TextView.class), mock(ImageView.class));
     }
 
-//    All this stuff is now private in our presenter because it hides the view logic it does, just updated the objects passed to it...
+    @Test
+    public void shouldUpdateDeckTitleInput() {
+        when(newDeckContext.getDeckTitle()).thenReturn(A_DECK_TITLE);
+        
+        presenter.updateDeckTitleInput();
+
+        verify(deckTitleInput).setText(A_DECK_TITLE);
+    }
+
+
+    //    All this stuff is now private in our presenter because it hides the view logic it does, just updated the objects passed to it...
 //
 //    @Test
 //    public void nextButtonShouldBeClickableWhenThereIsADeckTitle() {
