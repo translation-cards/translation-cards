ad3943a83ea99caeb3ad7b4dcff20c90365fb6cb
Coleman Beasley <cbeasley@thoughtworks.com>
2016-08-31 16:01:26 -0700

    Coleman #257 - change abi version in emulator start script

--- Appium/createStartEmu.sh
+++ Appium/createStartEmu.sh
@@ -2,8 +2,8 @@
 
 EMULATOR_NAME=android23Emulator
 TARGET_NAME=android-23
-TARGET_ABI=google_apis/x86
-#TARGET_ABI=armeabi-v7a
+#TARGET_ABI=google_apis/x86
+TARGET_ABI=armeabi-v7a
 
 echo no | $ANDROID_HOME/tools/android create avd -n $EMULATOR_NAME -t $TARGET_NAME --abi $TARGET_ABI
 $ANDROID_HOME/platform-tools/adb start-server
