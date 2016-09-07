98cc5d891889e742973c9945519c553e12d42b8b
Coleman Beasley <cbeasley@thoughtworks.com>
2016-09-07 09:29:01 -0700

    Coleman #257 - use 32 bit emulator images

--- Appium/createStartEmu.sh
+++ Appium/createStartEmu.sh
@@ -2,7 +2,7 @@
 
 EMULATOR_NAME=android23Emulator
 TARGET_NAME=android-23
-TARGET_ABI=x86_64
+TARGET_ABI=x86
 #TARGET_ABI=armeabi-v7a
 
 echo no | $ANDROID_HOME/tools/android create avd -f -n $EMULATOR_NAME -t $TARGET_NAME --abi $TARGET_ABI
