17b275e7bfba9bbebd414a6b28e3a6337ec0b4b2
Pat Dale <pdale@thoughtworks.com>
2016-09-20 17:13:13 -0500

    Update LanguageSelectorActivity to use RecyclerView

--- app/src/main/java/org/mercycorps/translationcards/addDeck/activity/LanguageSelectorActivity.java
+++ app/src/main/java/org/mercycorps/translationcards/addDeck/activity/LanguageSelectorActivity.java
@@ -1,20 +1,25 @@
 package org.mercycorps.translationcards.addDeck.activity;
 
 import android.content.Intent;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
+import android.view.ViewGroup;
 import android.widget.EditText;
-import android.widget.ListView;
+import android.widget.TextView;
 
 import org.mercycorps.translationcards.MainApplication;
 import org.mercycorps.translationcards.R;
 import org.mercycorps.translationcards.activity.AbstractTranslationCardsActivity;
 import org.mercycorps.translationcards.service.LanguageService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.inject.Inject;
 
@@ -24,12 +29,12 @@ public class LanguageSelectorActivity extends AbstractTranslationCardsActivity {
     public static final String SELECTED_LANGUAGE_KEY = "selectedLanguage";
     public static final int CANCEL_BUTTON_ID = 0;
     @Bind(R.id.languages_list)
-    ListView languagesList;
+    RecyclerView languagesList;
     @Bind(R.id.language_filter_field)
     EditText languageFilterField;
     @Inject LanguageService languageService;
-    private ArrayAdapter<String> languagesAdapter;
     private String selectedLanguage;
+    private LanguageSelectorAdapter languageSelectorAdapter;
 
     @Override
     public void inflateView() {
@@ -41,16 +46,40 @@ public class LanguageSelectorActivity extends AbstractTranslationCardsActivity {
         MainApplication application = (MainApplication) getApplication();
         application.getBaseComponent().inject(this);
 
-        initLanguagesAdapter();
-        initLanguagesFilter();
-        initListView();
+        initializeLanguageList();
+        initializeLanguageFilterField();
+    }
+
+    private void initializeLanguageList() {
+        languagesList.setHasFixedSize(true);
+        languagesList.setLayoutManager(new LinearLayoutManager(this));
+        languageSelectorAdapter = new LanguageSelectorAdapter(languageService.getLanguageNames());
+        languagesList.setAdapter(languageSelectorAdapter);
+    }
+
+    private void initializeLanguageFilterField() {
+        TextWatcher textWatcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                languageSelectorAdapter.filter(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        };
+        languageFilterField.addTextChangedListener(textWatcher);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case CANCEL_BUTTON_ID:
-                selectedLanguage = null;
+                selectedLanguage = "";
                 finish();
                 break;
             default:
@@ -71,47 +100,70 @@ public class LanguageSelectorActivity extends AbstractTranslationCardsActivity {
     public void finish() {
         Intent data = new Intent();
         data.putExtra(SELECTED_LANGUAGE_KEY, selectedLanguage);
-        int result = selectedLanguage == null ? RESULT_CANCELED : RESULT_OK;
+        int result = selectedLanguage.isEmpty() ? RESULT_CANCELED : RESULT_OK;
         setResult(result, data);
         super.finish();
     }
 
-    private void initListView() {
-        languagesList.setAdapter(languagesAdapter);
-        languagesList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                selectedLanguage = languagesAdapter.getItem(position);
-                finish();
-            }
-        });
-    }
-
-    private void initLanguagesAdapter() {
-        languagesAdapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_list_item_1,
-                languageService.getLanguageNames());
-    }
-
-    private void initLanguagesFilter() {
-        TextWatcher textWatcher = new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                languagesAdapter.getFilter().filter(s);
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-            }
-        };
-        languageFilterField.addTextChangedListener(textWatcher);
-    }
-
     @Override
     public void setBitmapsForActivity() {
+    }
+
+    class LanguageSelectorAdapter extends RecyclerView.Adapter<LanguageSelectorAdapter.ViewHolder> {
+        private List<String> items;
+        private List<String> languageNames;
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+
+            public TextView languageItem;
+
+            public ViewHolder(TextView itemView) {
+                super(itemView);
+                languageItem = itemView;
+            }
+        }
+
+        public LanguageSelectorAdapter(List<String> languageNames) {
+            this.languageNames = languageNames;
+            this.items = languageNames;
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            TextView languageItem = (TextView) LayoutInflater.from(parent.getContext()).inflate(android.R.layout.simple_list_item_1, parent, false);
+            return new ViewHolder(languageItem);
+        }
+
+        @Override
+        public void onBindViewHolder(final ViewHolder holder, final int position) {
+            holder.languageItem.setText(items.get(position));
+            holder.languageItem.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    selectedLanguage = items.get(holder.getAdapterPosition());
+                    finish();
+                }
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            return items.size();
+        }
+
+        public void filter(String string) {
+            List<String> items = new ArrayList<>();
+            for (String languageName : languageNames) {
+                if (languageName.toLowerCase().contains(string.toLowerCase())) {
+                    items.add(languageName);
+                }
+            }
+            this.items = items;
+            notifyDataSetChanged();
+        }
+
+        public String getItem(int position) {
+            return items.get(position);
+        }
     }
 }
--- app/src/test/java/org/mercycorps/translationcards/addDeck/activity/LanguageSelectorActivityTest.java
+++ app/src/test/java/org/mercycorps/translationcards/addDeck/activity/LanguageSelectorActivityTest.java
@@ -2,8 +2,7 @@ package org.mercycorps.translationcards.addDeck.activity;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.widget.ListAdapter;
-import android.widget.ListView;
+import android.support.v7.widget.RecyclerView;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -11,7 +10,6 @@ import org.junit.runner.RunWith;
 import org.mercycorps.translationcards.BuildConfig;
 import org.mercycorps.translationcards.MainApplication;
 import org.mercycorps.translationcards.R;
-import org.mercycorps.translationcards.addDeck.activity.LanguageSelectorActivity;
 import org.mercycorps.translationcards.dagger.TestBaseComponent;
 import org.mercycorps.translationcards.service.LanguageService;
 import org.mercycorps.translationcards.util.ActivityHelper;
@@ -26,7 +24,9 @@ import java.util.Arrays;
 import javax.inject.Inject;
 
 import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.mercycorps.translationcards.addDeck.activity.LanguageSelectorActivity.CANCEL_BUTTON_ID;
+import static org.mercycorps.translationcards.addDeck.activity.LanguageSelectorActivity.SELECTED_LANGUAGE_KEY;
 import static org.mockito.Mockito.when;
 import static org.robolectric.Shadows.shadowOf;
 
@@ -50,39 +50,53 @@ public class LanguageSelectorActivityTest {
 
     @Test
     public void shouldAddLanguagesFromLanguageServiceToAdapter() throws Exception {
-        ListView languagesList = (ListView) activity.findViewById(R.id.languages_list);
-        ListAdapter languagesListAdapter = languagesList.getAdapter();
-        languagesListAdapter.getCount();
-        assertEquals(languageService.getLanguageNames().size(), languagesListAdapter.getCount());
+        RecyclerView languagesList = (RecyclerView) activity.findViewById(R.id.languages_list);
+
+        assertEquals(languageService.getLanguageNames().size(), languagesList.getAdapter().getItemCount());
+    }
+
+    @Test
+    public void shouldFilterLanguagesInLanguageAdapter() {
+        RecyclerView languagesList = (RecyclerView) activity.findViewById(R.id.languages_list);
+
+        LanguageSelectorActivity.LanguageSelectorAdapter adapter = (LanguageSelectorActivity.LanguageSelectorAdapter) languagesList.getAdapter();
+        adapter.filter("Imag");
+
+        assertEquals(1, languagesList.getAdapter().getItemCount());
     }
 
     @Test
     public void shouldReturnDefaultLanguageValueWhenListItemSelected() throws Exception {
-        ListView languagesList = (ListView) activity.findViewById(R.id.languages_list);
+        RecyclerView languagesList = (RecyclerView) activity.findViewById(R.id.languages_list);
         ShadowActivity shadowActivity = shadowOf(activity);
 
-        shadowOf(languagesList).performItemClick(0);
+        // workaround robolectric recyclerView issue
+        // http://stackoverflow.com/questions/36039575/how-do-i-unit-test-recyclerview-linearlayoutmanager-in-robolectric
+        languagesList.measure(0,0);
+        languagesList.layout(0,0,100,1000);
+
+        languagesList.findViewHolderForAdapterPosition(0).itemView.performClick();
 
         Intent resultIntent = shadowActivity.getResultIntent();
         assertEquals(Activity.RESULT_OK, shadowActivity.getResultCode());
-        assertEquals(resultIntent.getStringExtra(LanguageSelectorActivity.SELECTED_LANGUAGE_KEY), "Farsi");
+        assertEquals(resultIntent.getStringExtra(SELECTED_LANGUAGE_KEY), "Farsi");
     }
 
     @Test
     public void shouldReturnCancelResultCodeWhenUserClicksCancel() throws Exception {
         ShadowActivity shadowActivity = shadowOf(activity);
 
-        activity.onOptionsItemSelected(new RoboMenuItem(LanguageSelectorActivity.CANCEL_BUTTON_ID));
+        activity.onOptionsItemSelected(new RoboMenuItem(CANCEL_BUTTON_ID));
 
         assertEquals(Activity.RESULT_CANCELED, shadowActivity.getResultCode());
-        assertNull(shadowActivity.getResultIntent().getStringExtra(LanguageSelectorActivity.SELECTED_LANGUAGE_KEY));
+        assertTrue(shadowActivity.getResultIntent().getStringExtra(SELECTED_LANGUAGE_KEY).isEmpty());
     }
 
     @Test
     public void shouldDisplayLanguagesFromLanguageServiceList() throws Exception {
-        ListView languagesList = (ListView) activity.findViewById(R.id.languages_list);
+        RecyclerView languagesList = (RecyclerView) activity.findViewById(R.id.languages_list);
 
-        String languageDisplayName = ((String)languagesList.getAdapter().getItem(0));
+        String languageDisplayName = ((LanguageSelectorActivity.LanguageSelectorAdapter) languagesList.getAdapter()).getItem(0);
 
         assertEquals("Farsi", languageDisplayName);
     }
