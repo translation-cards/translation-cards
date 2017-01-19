5184a3d322dd4078f5e40718a6632cce715aef56
Pat Dale <pdale@thoughtworks.com>
2017-01-19 12:12:30 -0600

    Nelson/Pat - Update target abi.

--- Appium/createStartEmu.sh
+++ Appium/createStartEmu.sh
@@ -3,7 +3,7 @@
 EMULATOR_NAME=android23Emulator
 TARGET_NAME=android-23
 #TARGET_ABI=x86
-TARGET_ABI=armeabi-v7a
+TARGET_ABI=google_apis/armeabi-v7a
 
 echo no | $ANDROID_HOME/tools/android create avd -f -n $EMULATOR_NAME -t $TARGET_NAME --abi $TARGET_ABI
 $ANDROID_HOME/platform-tools/adb start-server
