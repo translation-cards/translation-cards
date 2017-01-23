00d268b84bacc17460e5ab8da68f1b8bd5876c11
Pat Dale <pdale@thoughtworks.com>
2017-01-23 11:26:02 -0600

    Pat - Creates and starts android emulator in headless mode.

--- Appium/createStartEmu.sh
+++ Appium/createStartEmu.sh
@@ -3,11 +3,12 @@
 EMULATOR_NAME=android23Emulator
 TARGET_NAME=android-23
 #TARGET_ABI=x86
-TARGET_ABI=google_apis/armeabi-v7a
+TARGET_ABI=default/x86_64
 
+echo "y" | $ANDROID_HOME/tools/android update sdk -a --no-ui --filter 75
 echo no | $ANDROID_HOME/tools/android create avd -f -n $EMULATOR_NAME -t $TARGET_NAME --abi $TARGET_ABI
 $ANDROID_HOME/platform-tools/adb start-server
-$ANDROID_HOME/tools/emulator @$EMULATOR_NAME -no-window -no-audio &
-$ANDROID_HOME/platform-tools/adb wait-for-device
+$ANDROID_HOME/tools/emulator @$EMULATOR_NAME -no-window &
+./wait-for-emulator.sh
 
 exit
