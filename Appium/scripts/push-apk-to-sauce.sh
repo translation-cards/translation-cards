087d5b8d166034f86562b299ffd6941a7311eb5e
Nelson <nliao@thoughtworks.com>
2017-01-24 13:54:24 -0800

    Pat: updated push-apk-to-sauce.sh with valid env variable.

--- Appium/scripts/push-apk-to-sauce.sh
+++ Appium/scripts/push-apk-to-sauce.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-curl -u $SAUCE_USERNAME:$SAUCE_PASSWORD -X POST -H "Content-Type: application/octet-stream" https://saucelabs.com/rest/v1/storage/$SAUCE_USERNAME/app-debug.apk?overwrite=true --data-binary @app/build/outputs/apk/app-debug.apk
+curl -u $SAUCE_USERNAME:$SAUCE_ACCESS_KEY -X POST -H "Content-Type: application/octet-stream" https://saucelabs.com/rest/v1/storage/$SAUCE_USERNAME/app-debug.apk?overwrite=true --data-binary @app/build/outputs/apk/app-debug.apk
